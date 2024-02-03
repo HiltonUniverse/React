ee3c6d5a48afc3e34ff15f4fd8cdc97f43e5a51c
hiltonUniverse <hackshilton@gmail.com>
2024-02-03 22:06:45 +0100

    Conditional template and ternary operator for loading different css for differnet class name

--- src/App.js
+++ src/App.js
@@ -2,32 +2,35 @@ import "./App.css"
 import { useState } from "react";
 
 const initialState = [
-  { id: 1, name: "Recording Lecture", completed: true },
-  { id: 2, name: "Edit React Lecture", completed: false },
-  { id: 3, name: "Upload React Lecture", completed: false }
+  { id: 1, name: "Chapter first", completed: true },
+  { id: 2, name: "Chapter second", completed: false },
+  { id: 3, name: "Chapter third", completed: false }
 ]
 
 const App = () => {
-  const [tasks, setTask] = useState(
-    initialState
-  )
+  const [books, setBooks] = useState(initialState)
+  const [show, setShow] = useState(true)
 
-  function handleDelete(taskId) {
-    setTask(tasks.filter((task) => task.id !== taskId))
+  function handleRemove(bookId) {
+    setBooks(books.filter((book) => { return book.id !== bookId }))
   }
 
   return (
     <div className="App">
-      <h1>Task List</h1>
+      <h1>Book chapters</h1>
       <ul>
-        {tasks.map((task) => ( //iterate through each elemtn in the tasks array
-          <li key={task.id}> {/*give each list item an unique id*/}
-            <span>{task.id} - {task.name}</span> {/*pass arrow function to onclick function so the function handleDelete is called only*/}
-            <button onClick={() => { handleDelete(task.id) }} className="delete">Delete</button> {/*when the button is clicked*/}
-          </li>
-        ))}
+        <button onClick={() => setShow(!show)} className="trigger"> Toggle </button>
+        {/* if show is true, then only the list of book chapters is rendered otherwise not */}
+        {show &&
+          books.map((book) => {
+            return <li key={book.id} className={book.completed ? "completed" : "incomplete"}> {/*using ternary operator we can decide what css to apply!*/}
+              <span>{book.id}-{book.name}</span>
+              <button onClick={() => { handleRemove(book.id) }} className="delete"> Delete </button>
+            </li>
+          })
+        }
       </ul>
-    </div>
+    </div >
   )
 }
 
