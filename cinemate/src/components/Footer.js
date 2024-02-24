ce7549b1ea5022e6df3c40f376c977195431aed5
hilton khadka <hilton.khadka.mdl@gmail.com>
2024-02-24 20:39:39 +0100

    Cinemate - darkmode, navlink, footer and basic setup

--- cinemate/src/components/Footer.js
+++ cinemate/src/components/Footer.js
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom"
+
 export const Footer = () => {
   return (
     <footer>
-      
+      <div className="mx-auto p-4 md:flex md:justify-between border-b-2">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-500">© 2024 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved. </span>
+        <ul className="flex text-gray-500 text-sm item-center">
+          <li>
+              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+          </li>
+          <li>
+              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+          </li>
+          <li>
+              <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
+          </li>
+          <li>
+              <a href="#" target="_blank" className="hover:underline">Github</a>
+          </li>
+        </ul>
+    </div>
     </footer>
   )
 }
--- cinemate/src/pages/MovieList.js
+++ cinemate/src/pages/MovieList.js
@@ -1,7 +1,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="text-9xl bg-emerald-200 p-10 m-8 border-blue-700 border-8 rounded-lg">
+      <div>
         MovieList
       </div>
     </main>
