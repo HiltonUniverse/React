a5e850329b7536c0fd727348723b1183f074128e
hilton khadka <hilton.khadka.mdl@gmail.com>
2024-03-05 05:12:28 +0100

    Cinemate

--- cinemate/src/components/index.js
+++ cinemate/src/components/index.js
@@ -1,4 +1,7 @@
-export {Card} from "./Card"
+export {Card} from "./Card";
 
-export {Header} from "./Header"
-export {Footer} from "./Footer"
+export {Header} from "./Header";
+export {Footer} from "./Footer";
+
+export {Button} from "./Button";
+export {ScrollToTop} from "./ScrollToTop"
--- cinemate/src/routes/AllRoutes.js
+++ cinemate/src/routes/AllRoutes.js
@@ -5,12 +5,12 @@ export const AllRoutes = () => {
   return (
     <>
         <Routes>
-            <Route path="" element={<MovieList />} />
-            <Route path="/movie/:id" element={<MovieDetail />} />
-            <Route path="/movies/popular" element={<MovieList />} />
-            <Route path="/movies/top" element={<MovieList />} />
-            <Route path="/movies/upcoming" element={<MovieList />} />
-            <Route path="/movies/search" element={<Search />} />
+            <Route path="" element={<MovieList apiPath="movie/now_playing" title="Home"/>} />
+            <Route path="/movie/:id" element={<MovieDetail/> } />
+            <Route path="/movies/popular" element={<MovieList apiPath="movie/popular" title="Popular"/>} />
+            <Route path="/movies/top" element={<MovieList apiPath= "movie/top_rated" title="Top Rated"/>} />
+            <Route path="/movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming"/>} />
+            <Route path="search" element={<Search apiPath="search/movie"/>} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
     </>
